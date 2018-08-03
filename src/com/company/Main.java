5fa1d69631e8ed9816acd2b602b8e3c0f28e66d0
Lourdes Montes <monteslou18@gmail.com>
2018-08-03 18:03:43 -0500

    Sorting Method implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 
-    public static int[] sort(int [] input){
-        return new int[0];
+    public static int[] sort(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input.length - i - 1; j++) {
+                if (input[j] > input[j + 1]) {
+                    int temp = input[j];
+                    input[j] = input[j + 1];
+                    input[j + 1] = temp;
+                }
+            }
+        }
+        return input;
     }
 }
